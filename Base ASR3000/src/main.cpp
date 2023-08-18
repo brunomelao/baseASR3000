caaaf9efde4f344e087fcb4241949bb889bae0b7
Bruno Melão <114967953+brunomelao@users.noreply.github.com>
2023-08-18 16:29:42 -0300

    retirada de comentários com codigo antigo

--- Base ASR3000/src/main.cpp
+++ Base ASR3000/src/main.cpp
@@ -38,32 +38,17 @@ void onReceive(int packetSize)
   if (packetSize == 0)
     return; // Se nenhuma mesnagem foi recebida , retorna nada
 
-  int recipient = LoRa.read();       // Endereco de quem ta recebendo
-  byte sender = LoRa.read();         // Endereco do remetente
-  byte incomingMsgId = LoRa.read();  // Mensagem
+  // int recipient = LoRa.read();       // Endereco de quem ta recebendo
+  // byte sender = LoRa.read();         // Endereco do remetente
+  // byte incomingMsgId = LoRa.read();  // Mensagem
   
 
   DeserializationError err = deserializeJson(deserializedDoc, LoRa);
-  serializeJson(deserializedDoc, Serial);
-   
-  if (recipient != localAddress && recipient != 0xFF)
-  {
-    Serial.println(" This message is not for me.");
-    return; // skip rest of function
-  }
   deserializedDoc["Redundancia Paraquedas Estabilizador"] = deserializedDoc["PPE"];
   deserializedDoc["Comercial Paraquedas Estabilizador"] = deserializedDoc["PPE"];
   deserializedDoc["Comercial Paraquedas Principal"] =deserializedDoc["PPP"];
   deserializedDoc["RSSI"] = LoRa.packetRssi();
-
-  Serial.println(" Recebido do dispositivo : 0x" + String(sender, HEX));
-  Serial.println(" Enviado para : 0x" + String(recipient, HEX));
-  Serial.println("ID da mensagem : " + String(incomingMsgId));
-  Serial.println("Altitude: " + String(deserializedDoc["Altitude"].as<float>()));
-  Serial.println("Giroscopio(x):"+(String)deserializedDoc["Giroscopio(x)"].as<float>());
-  Serial.println("Tempo"+(String)deserializedDoc["Tempo"].as<float>());
-  Serial.println(" RSSI : " + String(LoRa.packetRssi()));
-  Serial.println(" Snr : " + String(LoRa.packetSnr()));
+  serializeJson(deserializedDoc, Serial);
   Serial.println();
 }
 
